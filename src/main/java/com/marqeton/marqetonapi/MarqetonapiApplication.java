b25b692ce7dd2173d7a902f81ef913ea2fb65959
KavyaHK <freelancerstwenty20@gmail.com>
2020-07-08 00:07:56 +0530

    marqetonapi project

---
+++ src/main/java/com/marqeton/marqetonapi/MarqetonapiApplication.java
@@ -0,0 +1,13 @@
+package com.marqeton.marqetonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarqetonapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarqetonapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marqeton/marqetonapi/MarqetonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marqeton.marqetonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarqetonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
